4ed686b26baa5d949f52ab8468b918b8852faf34
Casey Burgess <caseychristinaburgess@gmail.com>
2025-03-03 22:06:42 +0100

    fixed basic pipelines, still lot of checks to do exec. need to make it work with built in commands. need to make sure it manages signals

--- Exec/pipeline.c
+++ Exec/pipeline.c
@@ -14,14 +14,10 @@ int	launch_pipeline(t_data *data, t_command *commands, int num_cmds)
 	int	pipe_fds[2];
 	t_command	*current;
 	t_command	*prev;
-	current = commands;
+	int	i;
 
-	//pipe(pipe_fds);
-	
-	//launch_first_child_pipe(data, current, pipe_fds);
-	//launch_last_child_pipe(data, current->next, pipe_fds);
-	
-	int i = 0;
+	current = commands;
+	i = 0;
 	while (i < num_cmds)
 	{
 		if (i < num_cmds - 1)
@@ -37,6 +33,7 @@ int	launch_pipeline(t_data *data, t_command *commands, int num_cmds)
 		i++;
 	}
 
+	// WAIT FOR ALL
 	i = 0;
 	while (i < num_cmds)
 	{
@@ -47,19 +44,14 @@ int	launch_pipeline(t_data *data, t_command *commands, int num_cmds)
 		commands = commands->next;
 		i++;
 	}
-	// WAIT FOR ALL
+	data->status = get_child_exit_status(data->status);
 	return (0);
 }
 
 int	launch_first_child_pipe(t_data *data, t_command *cmd, int *pipe_fds)
 {
-	// PARENT
-	cmd->pipe_fds[0] = pipe_fds[0]; // represents the pipe it will WRITE INTO
+	cmd->pipe_fds[0] = pipe_fds[0]; // unused end of pipe
 	cmd->pipe_fds[1] = pipe_fds[1]; // represents the pipe it will WRITE INTO
-	// LAUNCH FORK
-		// (it will close pipe_fds[0] because it doesn't read from the pipe)
-		// manage other redirections if present in the tokens
-		// at end, dup std out to the cmd->pipe_fds[1];
 	cmd->pid = fork();
 	if (cmd->pid == -1)
 	{
@@ -70,10 +62,11 @@ int	launch_first_child_pipe(t_data *data, t_command *cmd, int *pipe_fds)
 	if (cmd->pid == 0)
 	{
 		close(cmd->pipe_fds[0]);
+		cmd->fds[1] = cmd->pipe_fds[1];
+		dup2(cmd->fds[1], STDOUT_FILENO);
+		close(cmd->fds[1]);
 		handle_redirections(data, cmd, cmd->fds);
 		ft_dprintf(data->log, PINK"%s: in:%i, out:%i\n", cmd->av[0], cmd->fds[0], cmd->fds[1]);
-		dup2(cmd->pipe_fds[1], STDOUT_FILENO);
-		close(cmd->pipe_fds[1]);
 		ft_dprintf(data->log, PINK"%s: in:%i, out:%i\n", cmd->av[0], cmd->fds[0], cmd->pipe_fds[1]);
 		if (cmd->ac == 0 && cmd->error == 0)
 			cmd->error = ER_NO_CMD;
@@ -85,20 +78,13 @@ int	launch_first_child_pipe(t_data *data, t_command *cmd, int *pipe_fds)
 		close_fds(cmd);
 		clean_up_exit(data, errno, NULL);
 	}
-	// PARENT
 	return (0);
 }
 
 int	launch_middle_child_pipe(t_data *data, t_command *cmd, int* pipe_fds, t_command *prev)
 {
-	// PARENT
 	cmd->pipe_fds[0] = pipe_fds[0]; // represents the pipe it will NOT NEED
 	cmd->pipe_fds[1] = pipe_fds[1]; // represents the pipe it will WRITE INTO
-	// LAUNCH FORK 
-	// (it will close pipe_fds[0])
-	// (it will put std input to prev->pipe_fds[0]);
-	// (it will put std output to pipe_fds[1]);
-	// PARENT
 	cmd->pid = fork();
 	if (cmd->pid == -1)
 	{
@@ -110,12 +96,14 @@ int	launch_middle_child_pipe(t_data *data, t_command *cmd, int* pipe_fds, t_comm
 	{
 		close(cmd->pipe_fds[0]);
 		close(prev->pipe_fds[1]);
-		handle_redirections(data, cmd, cmd->fds);
-		if (dup2(prev->pipe_fds[0], STDIN_FILENO) == -1)
+		cmd->fds[0] = prev->pipe_fds[0];
+		cmd->fds[1] = pipe_fds[1];
+		if (dup2(cmd->fds[0], STDIN_FILENO) == -1)
 			ft_dprintf(data->log, "%s: failed to dup stdin to prev pipe\n", cmd->av[0]);
-		close(prev->pipe_fds[0]);
-		dup2(cmd->pipe_fds[1], STDOUT_FILENO);
-		close(cmd->pipe_fds[1]);
+		close(cmd->fds[0]);
+		dup2(cmd->fds[1], STDOUT_FILENO);
+		close(cmd->fds[1]);
+		handle_redirections(data, cmd, cmd->fds);
 		if (cmd->ac == 0 && cmd->error == 0)
 			cmd->error = ER_NO_CMD;
 		set_command_path(data, cmd->path, cmd->av[0], cmd);
@@ -135,11 +123,6 @@ int	launch_last_child_pipe(t_data *data, t_command *cmd, int *pipe_fds)
 {
 	// PARENT
 	cmd->pipe_fds[0] = pipe_fds[0];
-	// LAUNCH FORK
-	// (it will not close pipe_fds[1] because it already was closed in the parent);
-	// (it will manage any redirections);
-	// (it will put std intput to pipe_fds[0]);
-	// PARENT
 	cmd->pid = fork();
 	if (cmd->pid == -1)
 	{
@@ -149,14 +132,13 @@ int	launch_last_child_pipe(t_data *data, t_command *cmd, int *pipe_fds)
 	}
 	if (cmd->pid == 0)
 	{
+		cmd->fds[0] = pipe_fds[0];
 		close(pipe_fds[1]);
 		ft_dprintf(data->log, "-----last child pipe-----\n");
-		handle_redirections(data, cmd, cmd->fds);
-		ft_dprintf(data->log, PINK"%s: in:%i, out:%i\n"RESET, cmd->av[0], cmd->fds[0], cmd->fds[1]);
-		if (dup2(cmd->pipe_fds[0], STDIN_FILENO) == -1)
+		if (dup2(cmd->fds[0], STDIN_FILENO) == -1)
 			ft_dprintf(data->log, "%s: failed to dup stdin to prev pipe\n", cmd->av[0]);
-		close(cmd->pipe_fds[0]);
-		ft_dprintf(data->log, PINK"%s: in:%i, out:%i\n"RESET, cmd->av[0], cmd->pipe_fds[0], cmd->fds[1]);
+		close(cmd->fds[0]);
+		handle_redirections(data, cmd, cmd->fds);
 		if (cmd->ac == 0 && cmd->error == 0)
 			cmd->error = ER_NO_CMD;
 		set_command_path(data, cmd->path, cmd->av[0], cmd);
@@ -167,7 +149,6 @@ int	launch_last_child_pipe(t_data *data, t_command *cmd, int *pipe_fds)
 		close_fds(cmd);
 		clean_up_exit(data, errno, NULL);
 	}
-	// PARENT
 	close(pipe_fds[0]);
 	close(pipe_fds[1]);
 	return (0);
